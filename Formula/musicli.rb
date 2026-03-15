bb07701e77a0216ea922f1acde2eaf61de5d13b3
Anirud Lappathi <alappathi@x.ai>
2026-03-14 19:58:47 -0700

    v0.1.1

--- Formula/musicli.rb
+++ Formula/musicli.rb
@@ -12,24 +12,24 @@
 class Musicli < Formula
   desc "Terminal music player for Spotify with visualizer and album art"
   homepage "https://github.com/anirudlappathi/musicli"
-  version "0.1.0"
+  version "0.1.1"
   license "MIT"
 
   on_macos do
     on_arm do
       url "https://github.com/anirudlappathi/musicli/releases/download/v#{version}/musicli-darwin-aarch64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_AFTER_FIRST_RELEASE"
+      sha256 "f8e92627f7155add4415ed787070a20c31e62267bb0623a1f753f34f7cfd0fbb"
     end
     on_intel do
       url "https://github.com/anirudlappathi/musicli/releases/download/v#{version}/musicli-darwin-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_AFTER_FIRST_RELEASE"
+      sha256 "9a9bf32c5d9b54e1059193729679c1678138baaac24cca617426a2661699c2e3"
     end
   end
 
   on_linux do
     on_intel do
       url "https://github.com/anirudlappathi/musicli/releases/download/v#{version}/musicli-linux-x86_64.tar.gz"
-      sha256 "REPLACE_WITH_SHA256_AFTER_FIRST_RELEASE"
+      sha256 "4de3a36bd24cf258787bed6d9d470b1e9535b2a7b4dd917bff2ab4648dbe8fdc"
     end
   end
 
